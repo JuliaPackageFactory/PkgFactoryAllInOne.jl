7afa5386e1c68138051debb20ea2a4b87005993f
ohno <59360244+ohno@users.noreply.github.com>
2026-09-23 21:21:35 +0000

    Update TemplateAllInOne from PkgFactory 73aff84df1346b46f7d5b6d0f57bb1a03ae8270b

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(;
         prettyurls = get(ENV, "CI", "false") == "true",
         canonical = "https://JuliaPackageFactory.github.io/TemplateAllInOne.jl",
         edit_link = "main",
-        assets = String[],
+        assets = ["assets/custom.css"],
     ),
     pages = [
         "Home" => "index.md",
